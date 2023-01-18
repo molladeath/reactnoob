b1ebf90d45a83aaf49fa8c158def3064c084ccdd
molladeath <molladeath@gmail.com>
2023-01-18 20:37:49 +0900

    모르겠다능

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
+
 
 function Banner(props) {
     return (
-        <div style={{height : "100vh"}}>
-            
-        </div>
+        <div id={props.objid} className='h-100'>
+            <Swiper
+        modules={[ Pagination, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={2}
+        centeredSlides={true}
+        autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+        }}
+        pagination={{
+            type : "fraction"
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        >
+        <SwiperSlide className='d-flex flex-colum
+        justify-content-center align-items-center' 
+        style={{height : '800px'}}>양</SwiperSlide>
+        <SwiperSlide className='d-flex flex-colum
+        justify-content-center align-items-center' 
+        style={{height : '800px'}}>현</SwiperSlide>
+        <SwiperSlide className='d-flex flex-colum
+        justify-content-center align-items-center' 
+        style={{height : '800px'}}>영</SwiperSlide>
+        </Swiper>
+    </div>
     );
 }
 
--- src/component/Maincomp.js
+++ src/component/Maincomp.js
@@ -4,7 +4,7 @@ import Banner from './Banner';
 function maincomp(props) {
     return (
         <div>
-            <Banner></Banner>
+            <Banner objid='maincomp'></Banner>
             <div>
                 <div style={{height : '100vh'}} id='process' className='border-top'></div>
                 <div style={{height : '100vh'}} id='review' className='border-top bg-dark'></div>
