a0b3b26467755a5c28adbc40d03ab0e9bb2a9dcd
molladeath <molladeath@gmail.com>
2023-01-18 20:38:34 +0900

    커밋하라능

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -5,9 +5,35 @@ import { Pagination, Autoplay } from 'swiper';
 // Import Swiper styles
 import 'swiper/css';
 import "swiper/css/pagination";
+import mbanner from "./css/mbanner.module.css";
 
 
 function Banner(props) {
+    const bannerdb = {
+        mainBanner :[
+            {
+                title : ['h2태그에 삽입되는 큰 타이틀','p태크에 들어가는 설명문구'],
+                type : 'a',
+                href : ['http://www.github.com', '_black'],
+                cls : ['event','bg-light']
+            },
+            {
+                title : ['두번째기획배너이름','작은 설명문구'],
+                type : 'Link',
+                href : ['/promotion2', ''],
+                cls : ['project','bg-danger']
+            },
+            {
+                title : ['세번째기획배너이름','작은 설명문구'],
+                type : 'Link',
+                href : ['/promotion3', ''],
+                cls : ['project','bg-info']
+            }
+        ]
+    }
+
+
+
     return (
         <div id={props.objid} className='h-100'>
             <Swiper
@@ -25,16 +51,23 @@ function Banner(props) {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         >
-        <SwiperSlide className='d-flex flex-colum
-        justify-content-center align-items-center' 
-        style={{height : '800px'}}>양</SwiperSlide>
-        <SwiperSlide className='d-flex flex-colum
-        justify-content-center align-items-center' 
-        style={{height : '800px'}}>현</SwiperSlide>
-        <SwiperSlide className='d-flex flex-colum
-        justify-content-center align-items-center' 
-        style={{height : '800px'}}>영</SwiperSlide>
-        </Swiper>
+     {
+        bannerdb.mainBanner.map((value,index)=>{
+            return (
+                <SwiperSlide className={mbanner.item +" "+value.cls.join(" ")}>
+                <div>
+                    <h2>
+                        {value.title[0]}
+                    </h2>
+                    <p>
+                        {value.title[1]}
+                    </p>
+                </div>
+            </SwiperSlide>
+        )
+        })
+       
+    }</Swiper>  
     </div>
     );
 }
